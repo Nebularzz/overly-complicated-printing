a30fbb12eb9986340c99e628422cf025cbad8263
Nebular <103582504+Nebularzz@users.noreply.github.com>
2022-11-11 16:58:27 +0100

    a

--- script.lua
+++ script.lua
@@ -2,7 +2,7 @@ local uis = game:GetService("UserInputService")
 
 local ram = {}
 local vram = {}
-local c = {"print"}
+local c = {"print","add"}
 
 function vraminsert(add)
 	table.insert(vram,add)
@@ -10,6 +10,12 @@ end
 
 function vramremove(add)
 	table.remove(vram,add)
+end
+
+function flushmem()
+	for i,v in ram do
+		table.remove(ram,1)
+	end
 end
 
 function display(str)
@@ -34,6 +40,10 @@ uis.InputBegan:Connect(function(input)
 		local action = command[1]
 		local arg = command[2]
 		
+		local calc = command[1]
+		local n1 = command[2]
+		local n2 = command[3]
+		
 		if action == c[1] then
 			table.insert(ram,arg)
 			
@@ -41,6 +51,48 @@ uis.InputBegan:Connect(function(input)
 				if v == arg then
 					display(v)
 					table.remove(ram,i)
+				end
+			end
+		end
+		
+		
+		if calc == c[3] then
+			table.insert(ram, n1)
+			table.insert(ram,n2)
+			
+			for i,v in ram do
+				if v == n1 then
+					for i,v2 in ram do
+						if v2 == n2 then
+							local a = tonumber(v)
+							table.insert(ram,a)
+							local a2 = tonumber(v2)
+							table.insert(ram,v2)
+							
+							for i,n in ram do
+								if n == a2 then
+									for i,b in ram do
+										if b == a then
+											table.insert(ram,n+b)
+											for i,aa in ram do
+												if aa == n+b then
+													display(aa)
+													flushmem()
+													for k,vr in vram do
+														print("a")
+														vramremove(k)
+													end
+												end
+											end
+										end
+									end
+									
+									print("calculating")
+									
+								end
+							end
+						end
+					end
 				end
 			end
 		end
